26dc24d9a93baf8b63ac32cbe0d4eb2c73ebe278
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-05-05 17:38:20 +0200

    add ugly lights list draft

--- light_master_app/lib/widgets/home.dart
+++ light_master_app/lib/widgets/home.dart
@@ -9,25 +9,46 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         child: Consumer<AppModel>(builder: (context, model, child) {
-      return Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(mainAxisSize: MainAxisSize.min, children: [
-                Text("this is an example with ${model.lightSources.length}.")
-              ]),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextButton(
-                      onPressed: () =>
-                          model.addLightSource(LightSource("1.1.1.1", "LS x")),
-                      child: Text("click me I dare you"))
-                ],
-              )
-            ],
-          ));
+      final lightSources = model.lightSources;
+      return CustomScrollView(
+        semanticChildCount: lightSources.length + 2,
+        slivers: [
+          const CupertinoSliverNavigationBar(largeTitle: Text('Lights')),
+          SliverSafeArea(
+            top: false,
+            minimum: const EdgeInsets.only(top: 8),
+            sliver: SliverList(
+                delegate: SliverChildBuilderDelegate((context, index) {
+              if (index < lightSources.length) {
+                // TODO: return light widgets
+                return Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red[500]),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Text("Lights '$index'"),
+                );
+              } else if (index == lightSources.length) {
+                // for testing purposes
+                return Row(mainAxisSize: MainAxisSize.min, children: [
+                  Text("this is an example with ${model.lightSources.length}.")
+                ]);
+              } else if (index == lightSources.length + 1) {
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextButton(
+                        onPressed: () => model
+                            .addLightSource(LightSource("1.1.1.1", "LS x")),
+                        child: Text("click me I dare you"))
+                  ],
+                );
+              } else {
+                return null;
+              }
+            })),
+          )
+        ],
+      );
     }));
   }
 }
