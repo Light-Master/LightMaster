0445d9144b72de6916204a746429eac3accac36f
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-05-06 14:23:48 +0200

    add light picker

--- light_master_app/lib/widgets/mono_light_settings.dart
+++ light_master_app/lib/widgets/mono_light_settings.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-class MonoLightSettings extends StatelessWidget {
+class MonoLightSettings extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MonoLightSettingsState();
+}
+
+class _MonoLightSettingsState extends State<MonoLightSettings> {
+  Color selectedColor = Colors.red;
+
   @override
   Widget build(BuildContext context) {
-    return Text("Mono light settings");
+    return Scaffold(
+        body: Material(
+            child: ColorPicker(
+                pickerColor: selectedColor,
+                onColorChanged: (newColor) =>
+                    setState(() => selectedColor = newColor))));
   }
 }
