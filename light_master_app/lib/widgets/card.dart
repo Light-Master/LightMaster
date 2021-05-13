22a02b2d5a95cb223ea0d980ba5fd4c7fd6d7f21
S2010455004 <S2010455004@students.fh-hagenberg.at>
2021-05-14 00:43:10 +0200

    new card creation reacts on shadow and is turned off upon creation, need to get colors from popup choice

--- light_master_app/lib/widgets/card.dart
+++ light_master_app/lib/widgets/card.dart
@@ -10,7 +10,8 @@ class LMCard extends StatelessWidget {
   final double turnedOnElevation = 12;
   final double turnedOffElevation = 0;
   final double childPadding = 45;
-  final Color shadowColor = Colors.transparent;
+  final Color shadowColor = Colors.blue;
+  double elevation;
 
   @override
   Widget build(BuildContext context) {
@@ -32,86 +33,19 @@ class LMCard extends StatelessWidget {
                   });
             },
             child: Container(
-                // elevation: lightSource.isTurnedOn ? this.turnedOnElevation : this.turnedOffElevation,
                 decoration: BoxDecoration(
-                    color: lightSource.isTurnedOn ? Colors.blue : Colors.red,
-                    borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(35),
-                        topRight: const Radius.circular(35))),
-                child: Text(lightSource.name))));
-  }
-}
-
-// callback
-typedef void TurnedOnCallback(bool state);
-
-// father
-class Cards extends StatefulWidget {
-  int idx;
-  Color selectedColor;
-  bool isTurnedOn;
-  Cards(int idx, Color selectedColor, bool isTurnedOn) {
-    this.idx = idx;
-    this.selectedColor = selectedColor;
-    this.isTurnedOn = isTurnedOn;
-  }
-
-  @override
-  State<StatefulWidget> createState() => _CardsState();
-}
-
-// son
-class Card extends StatelessWidget {
-  TurnedOnCallback setStateColor;
-  Card({@required this.setStateColor});
-
-  @override
-  Widget build(BuildContext context) {
-    double elevation = 12;
-    final double childPadding = 45;
-    Color cardColor;
-    Color shadowColor;
-
-    setStateColor(false);
-    return ImageCard(
-      elevation: elevation,
-      childPadding: childPadding,
-      color: cardColor,
-      shadowColor: shadowColor,
-      image: Image.network(
-        'https://miro.medium.com/max/85/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
-      ),
-    );
-  }
-}
-
-class _CardsState extends State<Cards> {
-  double elevation = 12;
-  final double childPadding = 45;
-  Color cardColor;
-  Color shadowColor;
-
-  void setStateColor() {
-    if (widget.isTurnedOn) {
-      cardColor = widget.selectedColor;
-      shadowColor = widget.selectedColor;
-      elevation = 12;
-    } else {
-      cardColor = Colors.grey;
-      shadowColor = Colors.transparent;
-      elevation = 0;
-    }
-    print(
-        "changing state color\ncolor=$cardColor\nshadow=$shadowColor\nelevation=$elevation");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Card(
-      setStateColor: (bool state) {
-        widget.isTurnedOn = state;
-        print("inside son with state $state");
-      },
-    ));
+                  color:
+                      lightSource.isTurnedOn ? Colors.transparent : shadowColor,
+                ),
+                child: ImageCard(
+                  elevation: elevation = lightSource.isTurnedOn
+                      ? turnedOffElevation
+                      : turnedOnElevation,
+                  childPadding: childPadding,
+                  shadowColor: shadowColor,
+                  image: Image.network(
+                    'https://miro.medium.com/max/85/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
+                  ),
+                ))));
   }
 }
