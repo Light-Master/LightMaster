6c4624840b9445b9189e7cb47db045871619a4ec
coderluka <iluka.dimitrijevic@gmail.com>
2021-05-19 23:01:11 +0200

    removed notifier and consumer from home screen

--- light_master_app/lib/main.dart
+++ light_master_app/lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:light_master_app/core/models/app_model.dart';
 import 'package:light_master_app/widgets/app.dart';
-import 'package:provider/provider.dart';
-
 import 'widgets/app.dart';
 
 void main() {
   // MARK:- model is created from app_model.dart with child from app.dart
-  runApp(ChangeNotifierProvider(
-    create: (context) => AppModel(),
-    child: App(),
-  ));
+  runApp(App());
 }
