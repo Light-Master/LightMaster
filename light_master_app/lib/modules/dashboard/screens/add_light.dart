4dab85f7445c479c64ba4a5efe269b19fdbdf775
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-06-02 22:52:52 +0200

    restyle add widget add button

--- light_master_app/lib/modules/dashboard/screens/add_light.dart
+++ light_master_app/lib/modules/dashboard/screens/add_light.dart
@@ -3,60 +3,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_radio_button_group/flutter_radio_button_group.dart';
 import 'package:light_master_app/modules/dashboard/bloc/add_light_bloc.dart';
-import 'package:light_master_app/core/models/app_model.dart';
 import 'package:light_master_app/core/models/light.dart';
 import 'package:light_master_app/core/models/light_source.dart';
 import 'package:light_master_app/modules/dashboard/bloc/managed_light_source_bloc.dart';
 import 'package:light_master_app/modules/dashboard/events/managed_light_source_event.dart';
 
 class AddLight extends StatelessWidget {
-  ButtonStyle _buttonStyle = ButtonStyle(
-      minimumSize: MaterialStateProperty.all(Size.fromHeight(50)),
-      backgroundColor: MaterialStateProperty.all<Color>(Colors.white));
-  String _selected = "";
+  final addressEditingController = TextEditingController(text: 'IP');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called,
-    // for instance, as done by the _increment method above.
-    // The Flutter framework has been optimized to make
-    // rerunning build methods fast, so that you can just
-    // rebuild anything that needs updating rather than
-    // having to individually changes instances of widgets.
+    // final addLightBloc = BlocProvider.of<AddLightBloc>(context);
 
-    final _addLightBloc = BlocProvider.of<AddLightBloc>(context);
+    final managedLightSourceBloc =
+        BlocProvider.of<ManagedLightSourceBloc>(context);
 
-    TextEditingController _name = TextEditingController(text: 'Name');
-    TextEditingController _ip = TextEditingController(text: 'IP');
-    FlutterRadioButtonGroup _detected = FlutterRadioButtonGroup(
-        items: [
-          /*put your items here*/
-          "192.168.1.10",
-          "192.168.1.11",
-          "192.168.1.12",
-          "192.168.1.13",
-          "192.168.1.14",
-          "192.168.1.15",
-          "192.168.1.16",
-          //"192.168.1.17",
-          //"192.168.1.18",
-          //"192.168.1.19",
-          //"192.168.1.20",
-          //"192.168.1.21",
-          //"192.168.1.22",
-          //"192.168.1.23",
-          //"192.168.1.24",
-          //"192.168.1.25",
-          //"192.168.1.26",
-          //"192.168.1.27",
-          //"192.168.1.28",
-          //"192.168.1.29",
-          //"192.168.1.30",
-        ],
-        onSelected: (String selected) {
-          _selected = selected;
-        });
-    //_autoDetect();
+    // FlutterRadioButtonGroup detected = FlutterRadioButtonGroup(
+    //     items: [
+    //       /*put your items here*/
+    //       "192.168.1.10",
+    //       "192.168.1.11",
+    //       "192.168.1.12",
+    //       "192.168.1.13",
+    //       "192.168.1.14",
+    //       "192.168.1.15",
+    //       "192.168.1.16",
+    //       //"192.168.1.17",
+    //       //"192.168.1.18",
+    //       //"192.168.1.19",
+    //       //"192.168.1.20",
+    //       //"192.168.1.21",
+    //       //"192.168.1.22",
+    //       //"192.168.1.23",
+    //       //"192.168.1.24",
+    //       //"192.168.1.25",
+    //       //"192.168.1.26",
+    //       //"192.168.1.27",
+    //       //"192.168.1.28",
+    //       //"192.168.1.29",
+    //       //"192.168.1.30",
+    //     ],
+    //     onSelected: (String selected) {
+    //       _selected = selected;
+    //     });
+
     return Container(
         decoration: BoxDecoration(
             color: Color.fromARGB(255, 225, 225, 225),
@@ -64,12 +54,12 @@ class AddLight extends StatelessWidget {
                 topLeft: const Radius.circular(35),
                 topRight: const Radius.circular(35))),
         height: 500,
-        padding: EdgeInsets.only(bottom: 20),
         child: Column(children: <Widget>[
           Container(
+              height: 65,
               padding: EdgeInsets.only(top: 20, bottom: 10),
-              child: Text('Add new light',
-                  style: new TextStyle(color: Colors.black, fontSize: 25.0))),
+              child: Text('Add Light',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25))),
           Expanded(
               child: SingleChildScrollView(
             child: Column(children: <Widget>[
@@ -77,11 +67,6 @@ class AddLight extends StatelessWidget {
                   color: Color.fromARGB(255, 225, 225, 225),
                   padding: EdgeInsets.only(left: 10.0, right: 10.0),
                   child: Column(children: <Widget>[
-                    CupertinoTextField(
-                      padding: EdgeInsets.only(top: 10, bottom: 10),
-                      textAlign: TextAlign.center,
-                      controller: _name,
-                    ),
                     Divider(
                       color: Color.fromARGB(255, 225, 225, 225),
                       height: 10,
@@ -91,7 +76,8 @@ class AddLight extends StatelessWidget {
                     CupertinoTextField(
                       padding: EdgeInsets.only(top: 10, bottom: 10),
                       textAlign: TextAlign.center,
-                      controller: _ip,
+                      controller: addressEditingController,
+                      placeholder: "Address",
                     ),
                     Divider(
                       color: Color.fromARGB(255, 225, 225, 225),
@@ -104,41 +90,46 @@ class AddLight extends StatelessWidget {
                   color: Colors.white70,
                   padding: EdgeInsets.only(top: 20.0, left: 20.0, bottom: 10.0),
                   // TODO: add auto search function to locate nearby lights on the network
-                  child: _detected)
+                  child: Text("wow"))
             ]),
           )),
           Container(
-              padding: EdgeInsets.only(top: 20, bottom: 0, left: 20, right: 20),
-              child: ElevatedButton(
-                  style: _buttonStyle,
-                  onPressed: () {
-                    String ip;
-                    if (_ip.text == "IP")
-                      ip = _selected;
-                    else
-                      ip = _ip.toString();
+            color: Colors.white,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Container(
+                        padding: EdgeInsets.only(
+                            top: 10, bottom: 10, left: 12.5, right: 12.5),
+                        child: TextButton(
+                            child: Text('Add'),
+                            style: TextButton.styleFrom(
+                                primary: Colors.white,
+                                backgroundColor:
+                                    Color.fromARGB(255, 116, 128, 255)),
+                            onPressed: () {
+                              String address =
+                                  this.addressEditingController.text != ""
+                                      ? this.addressEditingController.text
+                                      : "Effect wow";
 
-                    final _managedLightSourceBloc =
-                        BlocProvider.of<ManagedLightSourceBloc>(context);
+                              // todo:
+                              // only pass network ip in addEvent, BloC can query
+                              // all configuration from repository later
+                              managedLightSourceBloc.add(
+                                  ManagedLightSourceAddEvent(LightSource(
+                                      address,
+                                      "Light name",
+                                      true,
+                                      SolidLight(Colors.blue[800]))));
 
-                    _managedLightSourceBloc.add(ManagedLightSourceAddEvent(
-                      LightSource("${ip}", "${_name.text}", true,
-                          SolidLight(Colors.blue[800])),
-                    ));
-                    /*
-                      model.addLightSource(
-                        LightSource(
-                            selected,
-                            "Light ${model.lightSources.length}",
-                            true,
-                            SolidLight(Colors.yellow)),
-                      );*/
-                    Navigator.pop(context);
-                  },
-                  child: Text('Save',
-                      style: new TextStyle(
-                          color: Color.fromARGB(255, 225, 225, 225),
-                          fontSize: 15.0))))
+                              Navigator.pop(context);
+                            })))
+              ],
+            ),
+          )
         ]));
   }
 }
--- light_master_app/lib/modules/dashboard/screens/light_settings_sheet.dart
+++ light_master_app/lib/modules/dashboard/screens/light_settings_sheet.dart
@@ -25,10 +25,12 @@ class LightSettingsSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _lightSettingsBloc = BlocProvider.of<LightSettingsSheetBloc>(context);
-    final _managedLightSourceBloc = BlocProvider.of<ManagedLightSourceBloc>(context);
+    final _managedLightSourceBloc =
+        BlocProvider.of<ManagedLightSourceBloc>(context);
     LightSettingsSheetEvent mode;
-    final settingsWidget = BlocBuilder<LightSettingsSheetBloc, LightSettingsSheetEvent>(
-    builder: (BuildContext context, LightSettingsSheetEvent state) {
+    final settingsWidget =
+        BlocBuilder<LightSettingsSheetBloc, LightSettingsSheetEvent>(
+            builder: (BuildContext context, LightSettingsSheetEvent state) {
       switch (state) {
         case LightSettingsSheetEvent.mono:
           mode = LightSettingsSheetEvent.mono;
@@ -37,43 +39,40 @@ class LightSettingsSheet extends StatelessWidget {
           mode = LightSettingsSheetEvent.effect;
           return EffectsLightSettings(lightSource);
       }
-    }
-    );
+    });
 
     return FractionallySizedBox(
-            heightFactor: 0.9,
-            alignment: Alignment.bottomCenter,
-            child: SizedBox.expand(
-                child: Container(
-                    child: Column(children: [
-                    BlocProvider.value(
-                    value: _managedLightSourceBloc,
-                    child: LightSettingsSheetHeader(lightSource)),
-              BlocProvider.value(
-                value: _lightSettingsBloc,
+        heightFactor: 0.9,
+        alignment: Alignment.bottomCenter,
+        child: SizedBox.expand(
+            child: Container(
+                child: Column(children: [
+          BlocProvider.value(
+              value: _managedLightSourceBloc,
+              child: LightSettingsSheetHeader(lightSource)),
+          BlocProvider.value(
+            value: _lightSettingsBloc,
+            child: BlocProvider.value(
+              value: _managedLightSourceBloc,
+              child: LightSettingsSheetNavigation(),
+            ),
+          ),
+          Expanded(
+              child: Container(
+                  padding: EdgeInsets.only(top: 2, bottom: 2),
+                  color: Color.fromARGB(255, 225, 225, 225),
                   child: BlocProvider.value(
-                    value: _managedLightSourceBloc,
-                    child:LightSettingsSheetNavigation(),
-                  ),
-              ),
-              Expanded(
-                  child: Container(
-                      padding: EdgeInsets.only(top: 2, bottom: 2),
-                      color: Color.fromARGB(255, 225, 225, 225),
-                      child: BlocProvider.value(
-                          value: _managedLightSourceBloc,
-                          child: settingsWidget)
-                  )),
-              LightSettingsSheetFooter(() {
-                // this is called when the cancel button is pressed
-                // there exists no call back since we apply all changes to the original instance
+                      value: _managedLightSourceBloc, child: settingsWidget))),
+          LightSettingsSheetFooter(() {
+            // this is called when the cancel button is pressed
+            // there exists no call back since we apply all changes to the original instance
 
-                // previous code:
-                // lightSource.networkAddress =
-                //     this.widget.lightSourceCopy.networkAddress;
-                // lightSource.name = this.widget.lightSourceCopy.name;
-                // lightSource.light = this.widget.lightSourceCopy.light;
-              })
-            ]))));
+            // previous code:
+            // lightSource.networkAddress =
+            //     this.widget.lightSourceCopy.networkAddress;
+            // lightSource.name = this.widget.lightSourceCopy.name;
+            // lightSource.light = this.widget.lightSourceCopy.light;
+          })
+        ]))));
   }
 }
