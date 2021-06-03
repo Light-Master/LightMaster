a69e9365e8bde85d263290c48c6fb7873e11612d
Philip <s1710306007@student.fh-hagenberg.at>
2021-06-03 19:52:33 +0200

    DiscoverDevices for iOS

--- light_master_app/lib/modules/dashboard/repositories/discover_devices.dart
+++ light_master_app/lib/modules/dashboard/repositories/discover_devices.dart
@@ -1,64 +1,82 @@
+import 'dart:async';
 import 'dart:developer';
-import 'dart:io';
 
+import 'package:bonsoir/bonsoir.dart';
 import 'package:flutter/material.dart';
 import 'package:light_master_app/modules/dashboard/models/light.dart';
 import 'package:light_master_app/modules/dashboard/models/light_source.dart';
 import 'package:light_master_app/modules/dashboard/repositories/wled_rest_client.dart';
-import 'package:multicast_dns/multicast_dns.dart';
 import 'package:http/http.dart' as http;
 
-Stream<List<LightSource>> discoverWLEDDevices() async* {
-  const String name = '_http._tcp';
-  final MDnsClient client = MDnsClient();
+/// Provider model that allows to handle Bonsoir discoveries.
+class BonsoirDiscoveryModel extends ChangeNotifier {
   final WledRestClient wled = WledRestClient(httpClient: http.Client());
-  List<LightSource> res = [];
+  static const String type = '_http._tcp';
 
-  // Start the client with default options.
-  await client.start();
-  await for (final PtrResourceRecord ptr in client
-      .lookup<PtrResourceRecord>(ResourceRecordQuery.serverPointer(name))) {
-    await for (final SrvResourceRecord srv in client.lookup<SrvResourceRecord>(
-        ResourceRecordQuery.service(ptr.domainName))) {
-      await for (final IPAddressResourceRecord ip
-          in client.lookup<IPAddressResourceRecord>(
-              ResourceRecordQuery.addressIPv4(srv.target))) {
-        try {
-          var state = await wled.fetchState(ip.address.address);
-          var mainseg = state['state']['seg'][state['state']['mainseg']];
-          res.add(LightSource(
-              ip.address.address,
-              state['info']['name'],
-              state['state']['on'],
-              mainseg['fx'] == 0
-                  ? SolidLight(Color.fromRGBO(mainseg['col'][0],
-                      mainseg['col'][1], mainseg['col'][2], 1))
-                  : EffectLight(mainseg['fx'], mainseg['bri'], mainseg['sx'])));
-          yield res;
-        } catch (e) {
-          log("${srv.name} is not a WLED-Instance");
-        }
-      }
-      await for (final IPAddressResourceRecord ip
-          in client.lookup<IPAddressResourceRecord>(
-              ResourceRecordQuery.addressIPv6(srv.target))) {
-        try {
-          var state = await wled.fetchState(ip.address.address);
-          var mainseg = state['state']['seg'][state['state']['mainseg']];
-          res.add(LightSource(
-              ip.address.address,
-              state['info']['name'],
-              state['state']['on'],
-              mainseg['fx'] == 0
-                  ? SolidLight(Color.fromRGBO(mainseg['col'][0],
-                      mainseg['col'][1], mainseg['col'][2], 1))
-                  : EffectLight(mainseg['fx'], mainseg['bri'], mainseg['sx'])));
-          yield res;
-        } catch (e) {
-          log("${srv.name} is not a WLED-Instance");
-        }
+  /// The current Bonsoir discovery object instance.
+  BonsoirDiscovery _bonsoirDiscovery;
+
+  /// Contains all discovered (and resolved) services.
+  final List<LightSource> _resolvedServices = [];
+
+  /// The subscription object.
+  StreamSubscription<BonsoirDiscoveryEvent> _subscription;
+
+  /// Creates a new Bonsoir discovery model instance.
+  BonsoirDiscoveryModel() {
+    start();
+  }
+
+  /// Returns all discovered (and resolved) services.
+  List<LightSource> get discoveredServices => List.of(_resolvedServices);
+
+  /// Starts the Bonsoir discovery.
+  Future<void> start() async {
+    if (_bonsoirDiscovery == null || _bonsoirDiscovery.isStopped) {
+      _bonsoirDiscovery = BonsoirDiscovery(type: type);
+      await _bonsoirDiscovery.ready;
+    }
+
+    await _bonsoirDiscovery.start();
+    _subscription = _bonsoirDiscovery.eventStream.listen(_onEventOccurred);
+  }
+
+  /// Stops the Bonsoir discovery.
+  void stop() {
+    _subscription?.cancel();
+    _subscription = null;
+    _bonsoirDiscovery?.stop();
+  }
+
+  /// Triggered when a Bonsoir discovery event occurred.
+  Future<void> _onEventOccurred(BonsoirDiscoveryEvent event) async {
+    if (event.service == null || !event.isServiceResolved) {
+      return;
+    }
+
+    if (event.type == BonsoirDiscoveryEventType.DISCOVERY_SERVICE_RESOLVED) {
+      ResolvedBonsoirService service = event.service;
+      try {
+        var state = await wled.fetchState(service.ip);
+        var mainseg = state['state']['seg'][state['state']['mainseg']];
+        _resolvedServices.add(LightSource(
+            service.ip,
+            state['info']['name'],
+            state['state']['on'],
+            mainseg['fx'] == 0
+                ? SolidLight(Color.fromRGBO(
+                    mainseg['col'][0], mainseg['col'][1], mainseg['col'][2], 1))
+                : EffectLight(mainseg['fx'], mainseg['bri'], mainseg['sx'])));
+        notifyListeners();
+      } catch (e) {
+        log("$service is not a WLED-Instance");
       }
     }
   }
-  client.stop();
+
+  @override
+  void dispose() {
+    stop();
+    super.dispose();
+  }
 }
